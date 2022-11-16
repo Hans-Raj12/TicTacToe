ab1bd4ea9a7016896b45827e01d28b739e906279
Hans Raj <hans63565@gmail.com>
2022-11-16 15:46:40 +0500

    created layout of the game using Grid layout

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    var toggle = false;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun check(view: View){
+
+    }
+}
